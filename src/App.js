121e8b8b35dfaa09d00753f4cc0f1ee9ace50abb
Olatunji Omisakin <msknolatunji@gmail.com>
2021-02-08 11:38:51 +0100

    Settling up the various Components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 import React from "react";
-const App = () => (<div>Hello World from App component.</div>)
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Hello World from the app component
+        <TodoInput />
+        <TodoList />
+      </div>
+    )
+  }
+}
 
 export default App;
