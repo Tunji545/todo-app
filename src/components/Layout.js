79cf1f65ef9db4ca84594a59bf759ea96ca9b0de
Olatunji Omisakin <msknolatunji@gmail.com>
2021-07-11 15:07:48 +0100

    Completed Material Ui Tutorial

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,13 +8,18 @@ import { ListItemIcon } from '@material-ui/core';
 import { ListItemText } from '@material-ui/core';
 import { SubjectOutlined, AddCircleOutlineOutlined } from '@material-ui/icons';
 import { useHistory, useLocation } from 'react-router-dom';
+import { AppBar, Toolbar } from '@material-ui/core';
+import { format } from 'date-fns';
+import MarioAv from '../images/mario-av.png';
+import { Avatar } from '@material-ui/core';
 
 const drawith = 240;
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   page: {
     backgroundColor: '#f9f9f9',
     width: '100%',
+    padding: theme.spacing(3),
   },
   drawer: {
     width: drawith,
@@ -28,7 +33,20 @@ const useStyles = makeStyles({
   active: {
     backgroundColor: '#f4f4f4',
   },
-});
+  heading: {
+    padding: theme.spacing(2),
+  },
+  appBar: {
+    width: `calc(100% - ${drawith}px)`,
+  },
+  toolbar: theme.mixins.toolbar,
+  date: {
+    flexGrow: 1,
+  },
+  mario: {
+    marginLeft: theme.spacing(2),
+  },
+}));
 
 const Layout = ({ children }) => {
   const classes = useStyles();
@@ -51,13 +69,22 @@ const Layout = ({ children }) => {
   return (
     <div className={classes.root}>
       {/* AppBar */}
+      <AppBar color='textSecondary' className={classes.appBar}>
+        <Toolbar>
+          <Typography className={classes.date}>
+            {format(new Date(), 'do MMMM Y')}
+          </Typography>
+          <Typography>Mario</Typography>
+          <Avatar src={MarioAv} className={classes.mario} />
+        </Toolbar>
+      </AppBar>
       <Drawer
         className={classes.drawer}
         variant='permanent'
         anchor='left'
         classes={{ paper: classes.drawPaper }}
       >
-        <div variant='h5'>
+        <div variant='h5' className={classes.heading}>
           <Typography>Note Card</Typography>
         </div>
 
@@ -67,7 +94,7 @@ const Layout = ({ children }) => {
               key={list.text}
               button
               onClick={() => history.push(list.path)}
-              className={Location.pathname == list.path ? classes.active : null}
+              className={Location.pathname === list.path && classes.active}
             >
               <ListItemIcon>{list.icon}</ListItemIcon>
               <ListItemText primary={list.text} />
@@ -75,7 +102,10 @@ const Layout = ({ children }) => {
           ))}
         </List>
       </Drawer>
-      <div className={classes.page}>{children}</div>
+      <div className={classes.page}>
+        <div className={classes.toolbar}></div>
+        {children}
+      </div>
     </div>
   );
 };
--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
-// import CardContent from '@material-ui/core/CardContent';
-import { CardContent, IconButton, Typography } from '@material-ui/core';
+import {
+  CardContent,
+  IconButton,
+  Typography,
+  makeStyles,
+} from '@material-ui/core';
 import { DeleteOutlined } from '@material-ui/icons';
+import { Avatar } from '@material-ui/core';
+import { blue, green, pink, yellow } from '@material-ui/core/colors';
+
+const useStyles = makeStyles({
+  test: {
+    border: (note) => {
+      if (note.body.category === 'work') {
+        return '1px solid red';
+      }
+    },
+  },
+  avatar: {
+    backgroundColor: (note) => {
+      if (note.body.category === 'work') {
+        return yellow[700];
+      } else if (note.body.category === 'reminder') {
+        return pink[500];
+      } else if (note.body.category === 'todo') {
+        return green[500];
+      } else {
+        return blue[500];
+      }
+    },
+  },
+});
 
 const NoteCard = ({ note, handleDelete }) => {
+  const classes = useStyles(note);
+
   return (
     <div>
-      <Card elevation={1}>
+      <Card elevation={1} className={classes.test}>
         <CardHeader
+          avatar={
+            <Avatar className={classes.avatar}>
+              {note.body.category[0].toUpperCase()}
+            </Avatar>
+          }
           action={
             <IconButton onClick={() => handleDelete(note.id)}>
               <DeleteOutlined />
